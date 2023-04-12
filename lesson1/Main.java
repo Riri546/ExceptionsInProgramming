7039d0f73a0898eddbac7b647e755352ae1cbc14
Лилия <l_loshakova123@mail.ru>
2023-04-12 17:15:20 +0300

    Sckech

--- lesson1/Main.java
+++ lesson1/Main.java
@@ -1,7 +1,17 @@
 package lesson1;
 
 public class Main {
-    public static void main(String[] args){
-         int a = 2;
+    public static void main(String[] args) {
+        try {
+            int a = 2;
+            int b = 3;
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            //Показ того, что идет не так и продолжение исполнение 
+            e.printStackTrace();
+        } catch(RuntimeException e){
+
+        }
+
     }
 }
