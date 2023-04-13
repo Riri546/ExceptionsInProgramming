252742bef98956ccea15d59c45825c21e85329be
Лилия <l_loshakova123@mail.ru>
2023-04-13 16:34:16 +0300

    Sckech

--- lesson1/Task1.java
+++ lesson1/Task1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Task1 {
    public static void main(String[] args) {
-        
+        System.out.println("Hello world");
+        Scanner s = new Scanner(System.in);
+        int a = 
+        int [] array = {1, 2, 3, 4, 5};
+        arrayN (array);
     }
+
+private static void arrayN(int[] array, int ) {
+
+}
+
+    
 }
