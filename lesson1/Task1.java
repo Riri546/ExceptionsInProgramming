137650cfd7407b7ab9dff36e7560f953ccc4e746
Лилия <l_loshakova123@mail.ru>
2023-04-13 17:44:03 +0300

    Sckech

--- lesson1/Task1.java
+++ lesson1/Task1.java
@@ -7,14 +7,14 @@ public class Task1 {
         System.out.print("> ");
         int a = s.nextInt();
         int[] array = { 1, 2, 3, 4, 5 };
-        int res = arrayN(array, a)
+        int result = arrayN(array, a);
 
-        switch (res){
+        switch (result){
             case -3 -> System.out.println("Массив не может быть null");
             case -4 -> System.out.println("Длина массива равна нулю");
             case -1 -> System.out.println("Массив меньше заданной длины");
             case -2 -> System.out.println("% d Элемент не найден" + a);
-            default -> System.out.printf("%d Имеет инлекс %b", a, res);
+            default -> System.out.printf("%d Имеет инлекс %b", a, result);
         }
 
         System.out.println(arrayN(array, a));
@@ -22,10 +22,14 @@ public class Task1 {
     }
 
     private static int arrayN(int[] array, int n) {
-        if (array == null)
+        if (array == null){
             return -3;
-        if (array.length == 0)
+        }
+           
+        if (array.length == 0){
             return -4;
+        }
+            
         if (array.length < 3)
             return -1;
         for (int i = 0; i < array.length; i++) {
