322971cbab4d857d50fff9b583fb40a400b46111
Лилия <l_loshakova123@mail.ru>
2023-04-13 16:41:01 +0300

    Sckech

--- lesson1/Task1.java
+++ lesson1/Task1.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class Task1 {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
-        int b = s.nextInt();
-        int [] array = {1, 2, 3, 4, 5};
-        arrayN (array, a);
+        int[] array = { 1, 2, 3, 4, 5 };
+        arrayN(array, a);
         s.close();
     }
 
-private static void arrayN(int[] array, int n1) {
+    private static int arrayN(int[] array, int n1) {
+        for (int i = 0; i < array.length; i++) {
 
-}
+        }
+        return n1;
+    }
 
-    
 }
