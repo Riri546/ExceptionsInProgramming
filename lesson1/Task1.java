fcbf973547d1d008f5cbd64c9cad146bff44154a
Лилия <l_loshakova123@mail.ru>
2023-04-13 17:08:44 +0300

    Sckech

--- lesson1/Task1.java
+++ lesson1/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         Scanner s = new Scanner(System.in);
-        System.out.println("> ");
+        System.out.print("> ");
         int a = s.nextInt();
         int[] array = { 1, 2, 3, 4, 5 };
         System.out.println(arrayN(array, a));
@@ -12,15 +12,18 @@ public class Task1 {
     }
 
     private static int arrayN(int[] array, int n) {
-        int result = 0;
-        if (array == null) return -3;
-        if (array.length == 0) return -4; 
-        if (array.length < 3) return -1;
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] == n) {
-                    result = i;
-                }
-                return result;
+3
+        if (array == null)
+            return -3;
+        if (array.length == 0)
+            return -4;
+        if (array.length < 3)
+            return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n) {
+                return i;
             }
-        } return result;
         }
+        return -2;
+    }
+}
