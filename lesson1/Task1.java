14971d95b4159cbb28943fc7b9926720927f2a70
Лилия <l_loshakova123@mail.ru>
2023-04-13 16:35:29 +0300

    Sckech

--- lesson1/Task1.java
+++ lesson1/Task1.java
@@ -4,7 +4,8 @@ public class Task1 {
    public static void main(String[] args) {
         System.out.println("Hello world");
         Scanner s = new Scanner(System.in);
-        int a = 
+        int a = s.nextInt();
+        int b = s.nextInt();
         int [] array = {1, 2, 3, 4, 5};
         arrayN (array);
     }
