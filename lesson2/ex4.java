23547dee42a04707973892e061acc898d9505910
Лилия <l_loshakova123@mail.ru>
2023-04-20 18:15:35 +0300

    Sckech

--- lesson2/ex4.java
+++ lesson2/ex4.java
@@ -1,5 +1,31 @@
 package lesson2;
 
 public class ex4 {
-    
+    public static void main(String[] args) {
+        InputStream inputStream;
+        try {
+            String[] strings = { "apple", "orange" };
+            String strings1 = strings[2];
+            test();
+            int a = 1 / 0;
+            inputStream = new FileInputStream("/broken_reference");
+        } catch (Throwable e) {
+            e.printStackTrace();
+            /*
+             * } catch (StackOverflowError error) {
+             * System.out.println("Что-то сломалось");
+             */
+        } finally {
+            System.out.println("Я все равно выполнился");
+        }
+        System.out.println("Я жив!");
+    }
+
+    private static void test() throws IOException {
+        File file = new File("1");
+        file.createNewFile();
+        FileReader reader = new FileReader(file);
+        reader.read();
+        test();
+    }
 }
