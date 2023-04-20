1b95a54dc7876de8dd284f1351cf76ac66b4a35d
Лилия <l_loshakova123@mail.ru>
2023-04-20 18:13:28 +0300

    Sckech

--- lesson2/ex2.java
+++ lesson2/ex2.java
@@ -1,5 +1,19 @@
 package lesson2;
 
 public class ex2 {
-    
+    public static void main(String[] args) {
+        int sum = sum2d(new String[][] { { "1", "2", "3", "4", "j", "6" }, { "1", "w" }, { "2", "7" } });
+        System.out.println(sum);
+    }
+
+    private static int sum2d(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                int val = Integer.parseInt(arr[i][j]);
+                sum += val;
+            }
+        }
+        return sum;
+    }
 }
