c80f13e3429bb5acc99160ddd6f6ab395bd80e22
Лилия <l_loshakova123@mail.ru>
2023-04-22 15:01:33 +0300

    Sckech

--- lesson2/ex2.java
+++ lesson2/ex2.java
@@ -10,7 +10,10 @@ public class ex2 {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < 5; j++) {
-                int val = Integer.parseInt(arr[i][j]);
+                try{
+                    int val = Integer.parseInt(arr[i][j]);
+                }
+                
                 sum += val;
             }
         }
