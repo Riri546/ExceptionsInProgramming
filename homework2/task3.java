4ccca9fd3bd4d141a1fb80137e899f8f828e28bc
Лилия <l_loshakova123@mail.ru>
2023-04-25 23:59:37 +0300

    Sckech

--- homework2/task3.java
+++ homework2/task3.java
@@ -1,5 +1,7 @@
 package homework2;
 
+import java.io.FileNotFoundException;
+
 public class task3 {
     public static void main(String[] args) throws Exception {
         try {
@@ -9,12 +11,12 @@ public class task3 {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!"); 
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так..."); 
         }
      }
      public static void printSum(Integer a, Integer b) throws FileNotFoundException {
