051507ca9e965e76866c096d8d6eaf4e5056ceec
Лилия <l_loshakova123@mail.ru>
2023-04-25 17:42:36 +0300

    Sckech

--- homework2/task1.java
+++ homework2/task1.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-        System.out.print("Введите дробное число, разделив целое от дробного с помощью символа '.': ");
+        System.out.print("Введите дробное число, разделив целое от дробного с помощью символа '.' : ");
         try (Scanner scan = new Scanner(System.in)) {
             String number = scan.nextLine();
-            while (true) {
+            while(true) {
                 try {
                     float result = Float.parseFloat(number);
                     System.out.print("Вы ввели число: " + result);
                     break;
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
-                    System.out.println("Вы ввели буквенный символ. Повторите ввод: ");
+                    System.out.print("Здесь должно быть дробное число, введенное через симавол '.' : ");
                     scan.next();
 
                     // } catch (Exception e) {
