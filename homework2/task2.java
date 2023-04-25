25bd3f2a317a91fa1425bcef205991d18242ab21
Лилия <l_loshakova123@mail.ru>
2023-04-25 23:37:36 +0300

    Sckech

--- homework2/task2.java
+++ homework2/task2.java
@@ -1,12 +1,13 @@
 package homework2;
 
 public class task2 {
-    try {
-        int d = 0;
-        double catchedRes1 = intArray[8] / d;
-        System.out.println("catchedRes1 = " + catchedRes1);
-     } catch (ArithmeticException e) {
-        System.out.println("Catching exception: " + e);
-     }
-     
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+         } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+         }    
+    }
 }
