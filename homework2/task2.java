8c1b5e616fb5dde7c8455c287d029191b892334f
Лилия <l_loshakova123@mail.ru>
2023-04-26 00:54:40 +0300

    Solution task

--- homework2/task2.java
+++ homework2/task2.java
@@ -2,14 +2,14 @@ package homework2;
 
 public class task2 {
     public static void main(String[] args) {
-        int [] array = new int [8];
+        int[] array = new int[8];
         try {
             int d = 0;
             double catchedRes1 = array[0] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
-         } catch (ArithmeticException e) {
+        } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
             System.out.println("Нельзя делить на ноль");
-         }    
+        }
     }
 }
--- homework2/task3.java
+++ homework2/task3.java
@@ -9,7 +9,7 @@ public class task3 {
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
-            int[] abc = {1, 2};
+            int[] abc = { 1, 2 };
             abc[3] = 9;
         } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
@@ -18,7 +18,7 @@ public class task3 {
             System.out.println("Указатель не может указывать на null!");
         } catch (Throwable e) {
             System.out.println("Что-то пошло не так...");
-        }  
+        }
     }
 
     public static void printSum(Integer a, Integer b) throws FileNotFoundException {
