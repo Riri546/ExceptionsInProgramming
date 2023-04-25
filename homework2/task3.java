1b61da5c1b405e6a28fcd130fe0aee32d4890243
Лилия <l_loshakova123@mail.ru>
2023-04-25 23:59:57 +0300

    Sckech

--- homework2/task3.java
+++ homework2/task3.java
@@ -14,13 +14,14 @@ public class task3 {
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
         } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!"); 
+            System.out.println("Указатель не может указывать на null!");
         } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так..."); 
+            System.out.println("Что-то пошло не так...");
         }
-     }
-     public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
         System.out.println(a + b);
-     }
-     
+    }
+
 }
