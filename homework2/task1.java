42dc1184b27b2604e59e1382cd79c736701168aa
Лилия <l_loshakova123@mail.ru>
2023-04-25 16:49:04 +0300

    Sckech

--- homework2/task1.java
+++ homework2/task1.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-        System.out.println();
-        Scanner scan = new Scanner(System.in);
+        System.out.println("введите дробное число");
+        try (Scanner scan = new Scanner(System.in)) {
+            float num = scan.nextFloat();
+            System.out.println("Вы ввели число: " + num);
+        }
+
     }
 
 }
