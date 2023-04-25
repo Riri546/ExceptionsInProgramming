3ca24fa2d3c9750dc3807977f06f22c51fcf746a
Лилия <l_loshakova123@mail.ru>
2023-04-26 00:17:14 +0300

    Sckech

--- homework2/task3.java
+++ homework2/task3.java
@@ -9,15 +9,16 @@ public class task3 {
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
-            int[] abc = { 1, 2 };
+            int[] abc = {1, 2};
             abc[3] = 9;
-        } catch (IndexOutOfBoundsException ex) {
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
             System.out.println("Массив выходит за пределы своего размера!");
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException e) {
             System.out.println("Указатель не может указывать на null!");
-        } catch (Throwable ex) {
+        } catch (Throwable e) {
             System.out.println("Что-то пошло не так...");
-        }
+        }  
     }
 
     public static void printSum(Integer a, Integer b) throws FileNotFoundException {
