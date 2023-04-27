d1d74413d9002a1db014e694ca159b98f1e1b5d5
Лилия <l_loshakova123@mail.ru>
2023-04-27 12:48:04 +0300

    Copied the code for editing

--- lesson3/task1.java
+++ lesson3/task1.java
@@ -1,5 +1,31 @@
 package lesson3;
 
 public class task1 {
-    
+    private static void rwLine(Path pathRead, Path pathWrite) {
+        BufferedReader in = null;
+        BufferedWriter out = null;
+        try {
+            in = Files.newBufferedReader(pathRead);
+            out = Files.newBufferedWriter(pathWrite);
+            out.write(in.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
