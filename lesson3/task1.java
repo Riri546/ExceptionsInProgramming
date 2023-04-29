fdf01312479659b12607374f9b24463cc7007516
Лилия <l_loshakova123@mail.ru>
2023-04-30 00:07:50 +0300

    Sckech

--- lesson3/task1.java
+++ lesson3/task1.java
@@ -6,37 +6,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class task1 { 
+public class task1 {
     public static void main(String[] args) {
         rwLine(null, null);
     }
-    
-    private static void rwLine (Path pathRead, Path pathWrite) {
-        // BufferedReader in = null;
-        // BufferedWriter out = null;
-        try(BufferedReader in = new BufferedReader(Files.newBufferedReader(pathRead)); BufferedWriter out = new BufferedWriter(Files.newBufferedWriter(pathWrite))){
-            // in = Files.newBufferedReader(pathRead);
-            // out = Files.newBufferedWriter(pathWrite);
+
+    private static void rwLine(Path pathRead, Path pathWrite) {
+        try (BufferedReader in = new BufferedReader(Files.newBufferedReader(pathRead));
+                BufferedWriter out = new BufferedWriter(Files.newBufferedWriter(pathWrite))) {
             out.write(in.readLine());
         } catch (IOException e) {
             e.printStackTrace();
-        // } finally {
-        //     try {
-        //         if (in != null) {
-        //             in.close();
-        //         }
-        //     } catch (IOException e) {
-        //         e.printStackTrace();
-        //     }
-
-        //     try {
-        //         if (out != null) {
-        //             out.close();
-        //         }
-        //     } catch (IOException e) {
-        //         e.printStackTrace();
-        //     }
         }
     }
 }
-
