043794c3a224fb39b6c5e8a9fe15254a2e14d22e
Лилия <l_loshakova123@mail.ru>
2023-04-30 19:23:16 +0300

    Sckech

--- lesson3/task2.java
+++ lesson3/task2.java
@@ -3,15 +3,15 @@ package lesson3;
 import java.io.IOException;
 
 public class task2 {
-    public static void main(String[] args) { 
-        try{
+    public static void main(String[] args) {
+        try {
             doSomething();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void doSomething() throws IOException{
+    private static void doSomething() throws IOException {
 
     }
 }
